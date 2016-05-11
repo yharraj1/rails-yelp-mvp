310c5847cd3c8d57e7e8abf7a93ec4029d595981
yassine harraj <yassine.harraj@hec.edu>
2016-05-11 11:51:04 +0200

    Added restaurant model

---
+++ db/migrate/20160511095019_create_restaurants.rb
@@ -0,0 +1,13 @@
+class CreateRestaurants < ActiveRecord::Migration
+  def change
+    create_table :restaurants do |t|
+      t.string :name
+      t.string :address
+      t.integer :phone_number
+      t.string :category
+      t.integer :reviews
+
+      t.timestamps null: false
+    end
+  end
+end
