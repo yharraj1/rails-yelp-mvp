9abc47ea2b41df593d058aff59aff66468d05f06
yassine harraj <yassine.harraj@hec.edu>
2016-05-11 12:19:11 +0200

    updated tables

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160511095019) do
+ActiveRecord::Schema.define(version: 20160511095528) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string   "name"
@@ -22,5 +22,15 @@ ActiveRecord::Schema.define(version: 20160511095019) do
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
   end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string   "content"
+    t.integer  "rating"
+    t.integer  "restaurant_id"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
+
+  add_index "reviews", ["restaurant_id"], name: "index_reviews_on_restaurant_id"
 
 end
