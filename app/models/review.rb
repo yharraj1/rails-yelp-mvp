3026836d97e1358ee6223049783424964815fe3d
yassine harraj <yassine.harraj@hec.edu>
2016-05-11 14:20:34 +0200

    Models are done

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,5 +2,6 @@ class Review < ActiveRecord::Base
   belongs_to :restaurant
   validates :content, presence: true
   validates :rating, presence: true
-  validates :rating, :inclusion => { :in => 0..5, :message => " should be between 0 to 5" }
+  validates :rating, numericality: { only_integer: true }, :inclusion => { :in => 0..5, :message => " should be between 0 to 5", allow_nil: false }
+  validates :restaurant, presence: true
 end
